4c750e592218953fbca6386f6aa1927c1eec3bd1
Arsalan Ayaz <arsalan.ayaz95@gmail.com>
2020-07-04 19:34:47 +0500

    Updated Name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./App.css";
 function App() {
   return (
     <GlobalProvider>
-      <Header text="Hisaab Kitaab" />
+      <Header text="Hysaab Kitaab" />
       <div className="container">
         <Balance />
         <IncomeExpense />
